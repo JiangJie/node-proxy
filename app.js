7c32f5c8045fff12858f45adc9a6df89da276634
JarvisJiang <jiang115jie@gmail.com>
2014-12-21 23:34:59 +0800

    add vhost support

--- app.js
+++ app.js
@@ -3,3 +3,26 @@
 var koa = require('koa');
 
 var app = koa();
+
+const PORT = 3000;
+
+app.use(function*(next) {
+  var hostname = this.hostname;
+  var path = this.path;
+
+  path = path.split('/').filter(function(item) {
+    return item !== '';
+  });
+  path.unshift(hostname);
+  path.unshift('vhost');
+  path.unshift('.');
+  path = path.join('/');
+  try {
+    yield require(path).call(this, next);
+  } catch(e) {
+    console.error('[%s] %s', hostname, e.message);
+    yield next;
+  }
+});
+
+app.listen(PORT);
